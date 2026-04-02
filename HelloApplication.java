b1c018a98bd82d6ddf836fcd719a1dfc9de05b96
Siratul Mostakim Arman <siratularman2@gmail.com>
2026-04-02 22:57:51 +0600

    Add files via upload

---
+++ HelloApplication.java
@@ -0,0 +1,20 @@
+package buet.com.demo3;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Database.initializeDatabase();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        stage.setTitle("Document Authentication Application");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
